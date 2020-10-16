96f8c6bf02aa72a3e1994c0a2858b4e81cb3dd78
SUDA Mikihiro <s@sudame.net>
2020-10-16 18:02:52 +0900

    fix: use escape

--- memolution-twitter-link/src/App.vue
+++ memolution-twitter-link/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <!-- HTMLでは & が特殊文字なので、代わりに &amp; を使います -->
     <a
-      href="https://twitter.com/intent/tweet?hashtags=sudame,twitter&text=hogehoge"
+      href="https://twitter.com/intent/tweet?hashtags=sudame,twitter&amp;text=hogehoge"
       target="_blank"
       >ツイートしろ</a
     >
